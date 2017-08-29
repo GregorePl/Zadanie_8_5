e3cb093b054763fbdaf10fc5a4368b8a42683263
gregveg <kuzniak.greg@gmail.com>
2017-08-29 08:41:08 +0200

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var a = 5;
+var h = 7;
+var areaOfTriangle = a*h/2;
+prompt ('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to?');
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + areaOfTriangle);
