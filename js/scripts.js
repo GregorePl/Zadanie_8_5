4d4ccfe3184663c7e9295e37211279a9ee660db6
gregveg <kuzniak.greg@gmail.com>
2017-08-29 20:19:45 +0200

    script.js updated

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,6 @@
 var a = 5;
 var h = 7;
 var areaOfTriangle = a*h/2;
-prompt ('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to?');
+prompt ('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to?')
+alert ('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal ' + areaOfTriangle);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + areaOfTriangle);
